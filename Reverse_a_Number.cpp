f14473c324b55633298e901bce8e19a1b4e3ecc1
Rahul <rahulkumarkeshri475@gmail.com>
2023-07-01 15:54:10 +0530

    A Program to Reverse the No. input by User.

---
+++ Reverse_a_Number.cpp
@@ -0,0 +1,33 @@
+/*Write a program in C++ to revers a number taken as input from the user.*/
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+//Function to reverse a number
+int reverse(int n)
+{
+    int i,size,rem,num,org;
+    num=0;
+    org=n;
+    size=to_string(n).length();
+    for(i=1;i<=size;i++)
+    {
+        rem=n%10;
+        num=num*10+rem;
+        n=n/10;
+    }
+
+    cout<<"Entered Number is:\t"<<org<<"\n"<<"After reversing the number is: \t"<<num;
+    return 0;
+};
+
+int main()
+{
+    int i;
+    cout<<"Enter the Number you want to reverse:\t";
+    cin>>i;
+    reverse(i);
+    getch();
+    return 0;
+}
